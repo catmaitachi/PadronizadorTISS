8498f2fc2c001e26b026a7e7da3c42d6364b0863
Isaias-Alves <isaias.alves@sga.pucminas.br>
2026-02-25 10:06:12 -0300

    docs: documentação dos aliases do frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
 })
